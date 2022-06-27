2d9048808d090620b352ccb518f9a650c93209c3
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-06-27 14:11:12 +0200

    Added errors handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express()
 
@@ -8,6 +9,11 @@ app.use(cors({
     origin: "http://localhost:3000",
 }))
 app.use(express.json());
+
+//Routes
+
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log("Listening on port http://localhost:3001")
