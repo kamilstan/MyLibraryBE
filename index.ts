efd873ee082d2ea2543ca954f5431c0803f555ca
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-06-29 23:16:07 +0200

    Add endpoints for methods getOne, finAll and insert

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "express-async-errors";
 import {handleError, ValidationError} from "./utils/errors";
 import rateLimit from "express-rate-limit";
+import {bookRouter} from "./routers/book.router";
 
 const app = express()
 
@@ -15,8 +16,8 @@ app.use(rateLimit({
     windowMs: 5 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes))
 }));
-//Routes
 
+app.use('/book', bookRouter);
 
 app.use(handleError);
 
---
+++ routers/book.router.ts
@@ -0,0 +1,21 @@
+import {Router} from "express";
+import {BookRecord} from "../records/book.record";
+
+export const bookRouter = Router()
+
+     .get('/search/:title?', async (req, res) => {
+         const books = await BookRecord.findAll(req.params.title ?? '')
+         res.json(books)
+     })
+
+    .get('/:id', async (req, res) => {
+        const book = await BookRecord.getOne(req.params.id)
+        res.json(book)
+    })
+
+    .post('/', async (req, res) => {
+        const book = new BookRecord(req.body);
+            await book.insert()
+        res.json(book)
+    })
+
