21b44510293a183c6bdf6c2966d6f21176eafb9f
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-07-14 20:54:47 +0200

    Move db connection into dotenv

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,11 @@
 import {createPool} from "mysql2/promise";
+import 'dotenv/config';
 
 export const pool = createPool({
-    host: "localhost" ,
-    user: "root" ,
-    password: "root",
-    database: "my_library",
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     namedPlaceholders: true,
     decimalNumbers: true,
 })
