aa1abbc921f9d58cc54f18bdf7c53ddda08a8380
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-07-15 00:00:00 +0200

    Add config example and moved DB connection to config

---
+++ config/config.example.ts
@@ -0,0 +1,9 @@
+// export const config = {
+//     dbHost: 'localhost',
+//     dbUser: 'user',
+//     dbPassword: 'password',
+//     database: 'database',
+//     corsOrigin: 'http://localhost:3000',
+//     namedPlaceholders: true,
+//     decimalNumbers: true,
+// }
--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,12 @@
 import {createPool} from "mysql2/promise";
-import 'dotenv/config';
+import {config} from "../config/config";
+
 
 export const pool = createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.database,
     namedPlaceholders: true,
     decimalNumbers: true,
 })
