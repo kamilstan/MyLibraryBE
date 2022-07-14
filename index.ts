b113823211b06ac9ec1b291d3c24f65eca2aa23d
kamilstangret@wp.pl <kamilstangret@wp.plgit config --global user.email kamilstangret@wp.pl>
2022-07-14 22:28:05 +0200

    Uncomment rate limiter

--- index.ts
+++ index.ts
@@ -17,10 +17,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(rateLimit({
-//     windowMs: 5 * 60 * 1000, // 15 minutes
-//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes))
-// }));
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes))
+}));
 
 const router = Router();
 
